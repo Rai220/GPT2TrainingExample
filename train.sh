8939cc7b050de0117d50edd20162c73fe63971ff
Konstantin Krestnikov <kk@vamapps.com>
2021-01-05 17:36:16 +0300

    .

--- train.sh
+++ train.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-python run_lm_finetuning.py \
+    #--do_eval \
+    #--eval_data_file='input_data/val_harry.txt'\
+
+python examples/run_lm_finetuning.py \
     --output_dir=output \
     --model_type=gpt2 \
     --model_name_or_path=gpt2-medium \
     --do_train \
-    --train_data_file='input_data/train_harry.txt' \
-    --do_eval \
-    --eval_data_file='input_data/val_harry.txt'\
+    --train_data_file='dataset.txt' \
     --overwrite_output_dir\
     --block_size=200\
     --per_gpu_train_batch_size=1\
