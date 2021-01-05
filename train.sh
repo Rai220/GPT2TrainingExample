605c476f696abbd6d85fb05c1a7cf635afdd779c
Konstantin Krestnikov <kk@vamapps.com>
2021-01-05 17:32:14 +0300

    Runner

---
+++ train.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+python run_lm_finetuning.py \
+    --output_dir=output \
+    --model_type=gpt2 \
+    --model_name_or_path=gpt2-medium \
+    --do_train \
+    --train_data_file='input_data/train_harry.txt' \
+    --do_eval \
+    --eval_data_file='input_data/val_harry.txt'\
+    --overwrite_output_dir\
+    --block_size=200\
+    --per_gpu_train_batch_size=1\
+    --save_steps 5000\
+    --num_train_epochs=2
