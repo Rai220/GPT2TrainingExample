d63baa6f7ef0b12b16c2b2fee465d1161f93da2b
Your Name <you@example.com>
2021-01-21 12:40:11 +0000

    New training features

--- train.sh
+++ train.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=/usr/local/cuda-11.2/lib64
+#export LD_LIBRARY_PATH=/usr/local/cuda-11.2/lib64
+export PATH=${PATH}:/usr/local/cuda-11.2/bin
 export CUDA_HOME=/usr/local/cuda-11.2
+
+rm -rf output/
+rm -rf runs/
+rm gpt2-large_cached*
 
 python examples/run_lm_finetuning.py \
     --output_dir=output \
@@ -11,11 +16,11 @@ python examples/run_lm_finetuning.py \
     --do_eval \
     --eval_data_file='dataset_mini.txt' \
     --overwrite_output_dir\
-    --block_size=10\
+    --block_size=50 \
     --per_gpu_train_batch_size=1\
-    --save_steps 5000 \
-    --num_train_epochs=3 \
-    --evaluate_during_training
+    --save_steps=1000 \
+    --num_train_epochs=1
+    # --evaluate_during_training
 
     # --fp16_opt_level=O3 \
     # --fp16 \
